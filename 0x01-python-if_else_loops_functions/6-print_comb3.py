3704d7e873524ef946a71fd2d423b5158eaa0b55
tomi1710 <2294@holbertonschool.com>
2020-12-08 22:59:58 +0000

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(9):
+for i in range(10):
     for j in range(10):
-        if j > i:
+        if j > i and i != 8:
             print("{:d}{:d}, ".format(i, j), end="")
 print("89")
