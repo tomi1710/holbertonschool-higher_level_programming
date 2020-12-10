292a030042abf742b90d859598e69385e30980c8
tomi1710 <2294@holbertonschool.com>
2020-12-10 16:08:10 +0000

    1 argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,11 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     if argc == 0:
         print("0 arguments.")
+    elif argc == 1:
+        print("{:d} argument:".format(argc))
+        for i in range(argc + 1):
+            if i != 0:
+                print("{:d}: {}".format(i, sys.argv[i]))
     else:
         print("{:d} arguments:".format(argc))
         for i in range(argc + 1):
