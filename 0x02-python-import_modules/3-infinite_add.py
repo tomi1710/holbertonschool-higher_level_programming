6f6f7924a8eb323917ef286f85b6053d2af5fa9c
tomi1710 <2294@holbertonschool.com>
2020-12-10 16:01:11 +0000

    if name

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-argc = len(sys.argv) - 1
-suma = 0
-if argc == 0:
-    print("0")
-else:
-    for i in range(1, argc + 1):
-        suma = suma + int(sys.argv[i])
-    print("{:d}".format(suma))
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    suma = 0
+    if argc == 0:
+        print("0")
+    else:
+        for i in range(1, argc + 1):
+            suma = suma + int(sys.argv[i])
+        print("{:d}".format(suma))
