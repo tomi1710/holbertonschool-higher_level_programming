7a3492166a3368caaabe27393fa1a3d9e850e88a
tomi1710 <2294@holbertonschool.com>
2020-12-15 15:38:33 +0000

    add ifs

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (my_list):
-        if (idx < 0):
-            return (None)
-        if (idx > len(my_list)):
-            return (None)
-        i = my_list[idx]
-        return (i)
+    if (idx < 0):
+        return (None)
+    if (idx > (len(my_list) - 1)):
+        return (None)
+    i = my_list[idx]
+    return (i)
