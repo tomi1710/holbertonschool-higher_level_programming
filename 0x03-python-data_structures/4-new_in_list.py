1b0d7812616bbcdaff4f1faf573a4cd63e75c5d2
tomi1710 <2294@holbertonschool.com>
2020-12-15 15:48:59 +0000

    add ifs and .copy()

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    my_list[idx] = element
-    return (my_list)
+    if (idx < 0):
+        return (my_list)
+    if (idx > (len(my_list) - 1)):
+        return (my_list)
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return (new_list)
