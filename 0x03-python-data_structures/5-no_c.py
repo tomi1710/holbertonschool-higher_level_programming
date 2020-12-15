01c472b574b083d36449fb4d430bafc8be37d001
tomi1710 <2294@holbertonschool.com>
2020-12-15 15:54:03 +0000

    correct translate

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.replace('c', '')
-    my_string = my_string.replace('C', '')
+    my_string = my_string.translate({ord('c'): None})
+    my_string = my_string.translate({ord('C'): None})
     return (my_string)
