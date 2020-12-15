36b8d496039a804cae8d39dfd4a51cc6e5d60710
tomi1710 <2294@holbertonschool.com>
2020-12-15 17:31:20 +0000

    first commit

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    if (len(tuple_b) == 1):
+        tuple_b = tuple_b[0], 0
+    if (len(tuple_b) < 1):
+        tuple_b = 0, 0
+
+    tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return (tuple_c)
