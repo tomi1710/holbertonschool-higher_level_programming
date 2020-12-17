b64afca341bc7379c78041ee5cf60a33eb6a7ce5
tomi1710 <2294@holbertonschool.com>
2020-12-17 19:54:48 +0000

    first commit

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_map(matrix=[]):
+    newmatrix = []
+    for i in range(len(matrix)):
+        newmatrix.append(list(map((lambda a: a * a), matrix[i])))
+    return (newmatrix)
