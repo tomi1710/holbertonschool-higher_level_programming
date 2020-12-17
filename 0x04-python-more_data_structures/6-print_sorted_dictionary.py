6539f5e500483e5ab7a53a24112e40bca80f57b0
tomi1710 <2294@holbertonschool.com>
2020-12-17 17:44:19 +0000

    fix pep8

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted(a_dictionary)
     for key, value in a_dictionary.items():
-        print ("{}: {}".format(key, value))
+        print("{}: {}".format(key, value))
