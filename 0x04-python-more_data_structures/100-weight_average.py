31640e92a7390f47da182b239e890716c0d12abd
tomi1710 <2294@holbertonschool.com>
2020-12-17 19:49:16 +0000

    first commit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    fsum = 0
+    lsum = 0
+
+    for i in range(len(my_list)):
+        fsum = fsum + (my_list[i][0] * my_list[i][1])
+    for i in range(len(my_list)):
+        lsum = lsum + my_list[i][1]
+
+    return (fsum / lsum)
