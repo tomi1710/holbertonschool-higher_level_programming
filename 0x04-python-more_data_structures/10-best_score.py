5c46f2637087412cb31c627e86e3108c4ce3e74a
tomi1710 <2294@holbertonschool.com>
2020-12-17 18:25:00 +0000

    fix name instead of element

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        mx = -10000000000
+        mx = -1000000
 
         for i in a_dictionary:
             if (mx < a_dictionary[i]):
                 mx = a_dictionary[i]
+                a = i
 
-        return (mx)
+        return (a)
     else:
         return (None)
