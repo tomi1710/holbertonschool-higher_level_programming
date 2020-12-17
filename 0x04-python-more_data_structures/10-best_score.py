9ce1fb761ebc6edecc5fc3221501f0209e12984c
tomi1710 <2294@holbertonschool.com>
2020-12-17 20:57:41 +0000

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary:
         mx = 0
         for i in a_dictionary:
             if (mx < a_dictionary[i]):
