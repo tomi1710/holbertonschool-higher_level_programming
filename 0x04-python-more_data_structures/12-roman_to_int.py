80e059d22fc0c7ee70e50bcb544d2cbe8e54405e
tomi1710 <2294@holbertonschool.com>
2020-12-17 19:36:36 +0000

    first commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -14,11 +14,13 @@ def roman_to_int(roman_string):
             if (roman_string[i + 1] == 'M'):
                 roman = roman + 900
                 i = i + 1
-            roman = roman + 500
+            else:
+                roman = roman + 500
         elif roman_string[i] == 'C':
             if (roman_string[i + 1] == 'D'):
                 roman = roman + 400
-            roman = roman + 100
+            else:
+                roman = roman + 100
         elif roman_string[i] == 'L':
             roman = roman + 50
         elif roman_string[i] == 'X':
