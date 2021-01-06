e741ea6771ffc7f13da06c2b03540c1fe33dc927
tomi1710 <2294@holbertonschool.com>
2021-01-06 02:25:44 +0000

    first commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """__init__"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size with value"""
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """print a square with #"""
+        if self.__size > 0:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print("")
+        else:
+            print("")
