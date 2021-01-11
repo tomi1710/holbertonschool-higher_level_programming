d6ac8b27034605acae213faf9f91c4fbe587257a
tomi1710 <2294@holbertonschool.com>
2021-01-11 18:20:52 +0000

    rm

--- 0x07-python-test_driven_development/0-add_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def add_integer(a, b=98):
-    if type(a) == float or type(a) == int:
-        if type(b) == float or type(b) == int:
-            return (a + b)
-        else:
-            raise TypeError("b must be an integer")
-    else:
-        raise TypeError("a must be an integer")
--- 0x07-python-test_driven_development/3-say_my_name.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def say_my_name(first_name, last_name=""):
-    if type(first_name) == str:
-        if type(last_name) == str:
-            print("My name is {} {}".format(first_name, last_name))
-        else:
-            raise TypeError("last_name must be a string")
-    else:
-        raise TypeError("first_name must be a string")
