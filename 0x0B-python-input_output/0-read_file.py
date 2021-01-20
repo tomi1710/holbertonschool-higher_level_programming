7dc0ced2a7e23f04a249fcac1cb5ea294d217c7c
tomi1710 <2294@holbertonschool.com>
2021-01-20 20:22:14 +0000

    pep8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" defines a function that reads a text file (UTF8) and prints it to stdout """
+""" defines a function that reads a text file (UTF8) and prints
+    it to stdout """
 
 
 def read_file(filename=""):
