895358ddfbdc38a8ae57162c101ee1900b51d111
tomi1710 <2294@holbertonschool.com>
2021-01-26 15:08:41 +0000

    add

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ defines a class base """
 import sys
+import json
 
 
 class Base:
@@ -22,3 +23,46 @@ class Base:
             raise ValueError("{} must be >= 0".format(name))
         elif value <= 0 and (name == "width" or name == "height"):
             raise ValueError("{} must be > 0".format(name))
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ def """
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ that writes the JSON string representation of list_objs to a file"""
+        listo = []
+        filename = cls.__name__ + ".json"
+        if list_objs is not None:
+            with open(filename, 'w') as f:
+                for a in list_objs:
+                    listo.append(cls.to_dictionary(a))
+                f.write(Base.to_json_string(listo))
+        else:
+            with open(filename, 'w') as f:
+                f.write(listo)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ def """
+        lista = []
+        if json_string is not None and json_string:
+            return json.loads(json_string)
+        else:
+            return lista
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set """
+        if (cls.__name__ == "Square"):
+            a = cls(1)
+        elif (cls.__name__ == "Rectangle"):
+            a = cls(1, 1)
+        a.update(**dictionary)
+        return a
+
+    def load_from_file(cls):
+        """ def """
