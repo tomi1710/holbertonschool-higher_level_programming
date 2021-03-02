7b55c3d4888884344a5c96f1987638f401e7b631
tomi1710 <2294@holbertonschool.com>
2021-03-02 18:33:43 +0000

    FIX

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
 -- creates the table unique_id on your MySQL server
 
-CREATE TABLE IF NOT EXISTS unique_id (unique_id INT(1)) ,
+CREATE TABLE IF NOT EXISTS unique_id (id INT(UNIQUE(1))) ,
 name VARCHAR(256) NOT NULL);
