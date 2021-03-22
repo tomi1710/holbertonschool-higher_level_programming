c6043144a13f2b28ac9fee085f9583c327bf268d
tomi1710 <2294@holbertonschool.com>
2021-03-22 18:31:44 +0000

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-let big = 0; i = 0; big2 = 0;
+let big = 0; let i = 0; let big2 = 0;
 const cero = 0;
 process.argv.forEach((val, index) => {
   i++;
-  if (big < val) {
+  if (big / 10 < val / 10) {
     big = val;
   }
 });
 process.argv.forEach((val, index) => {
-  if (big2 < val && val !== big) {
+  if (big2 / 10 < val / 10 && val / 10 !== big / 10) {
     big2 = val;
   }
 });
