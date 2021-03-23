60133093f0854a11a2d3242851aa7128e4b711f1
tomi1710 <2294@holbertonschool.com>
2021-03-23 17:39:08 +0000

    add

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Square = require('./5-square');
+
+class aaa extends Square {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      console.log(c.repeat(this.size));
+    }
+  }
+}
+module.exports = aaa;
