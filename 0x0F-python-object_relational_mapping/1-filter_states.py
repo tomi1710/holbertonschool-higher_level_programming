d13820500bd43383ef5b8412fff0de7825db8d33
tomi1710 <2294@holbertonschool.com>
2021-03-27 12:29:22 +0000

    fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """ lists all states from the database hbtn_0e_0_usa """
 
+from sys import argv
+import MySQLdb
 if __name__ == "__main__":
 
-    from sys import argv
-    import MySQLdb
     MY_H = "localhost"
     MY_U = argv[1]
     MY_P = argv[2]
     MY_D = argv[3]
     nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
     consulta = nuevaconexion.cursor()
-    consulta.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    consulta.execute("SELECT * FROM states ORDER BY id ASC")
     resultado = consulta.fetchall()
     for fila in resultado:
-        for fila[1][0] == 'N':
+        if fila[1][0] == 'N':
             print(fila)
     nuevaconexion.close()
+    
