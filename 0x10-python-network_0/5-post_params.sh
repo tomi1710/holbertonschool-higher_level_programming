f0b0a6e613e87f5f48c6847c795bddacf92ddd84
tomi1710 <2294@holbertonschool.com>
2021-04-08 23:50:46 +0000

    add

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -sX POST -F 'email=hr@holbertonschool.com' -F 'subject=I will always be here for PLD' $1
