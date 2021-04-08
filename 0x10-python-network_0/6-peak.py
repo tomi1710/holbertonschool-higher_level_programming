8a5e4e3e2945f5b33a83c8f75747293213b5a120
tomi1710 <2294@holbertonschool.com>
2021-04-08 21:10:35 +0000

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,16 +5,16 @@
 def find_peak(listt):
     """ def func """
     if len(listt) == 0:
-        return (None)
+        return None
     elif len(listt) == 1:
-        return (listt[0])
+        return listt[0]
     elif len(listt) == 2:
-        return (max(listt))
+        return max(listt)
     half = int(len(listt) / 2)
     middle = listt[half]
     if listt[half - 1] < middle > listt[half + 1]:
-        return (middle)
-    elif middle < listt[half - 1]:
-        return (find_peak(listt[:listt[half + 1]]))
+        return middle
+    elif middle < listt[half + 1]:
+        return find_peak(listt[middle:])
     else:
-        return (find_peak(listt[middle:]))
+        return find_peak(listt[:listt[half + 1]])
