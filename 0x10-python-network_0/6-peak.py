062d59944b744aa0609a7d76584bead4969da2ed
tomi1710 <2294@holbertonschool.com>
2021-04-08 19:35:31 +0000

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,10 @@ def find_peak(list_of_integers):
     """ def func """
     if len(list_of_integers) == 0:
         return (None)
+    if len(list_of_integers) == 1:
+        return (list_of_integers[0])
+    if len(list_of_integers) == 2:
+        return (max(list_of_integers))
     a = list_of_integers[1]
     for i in range(2 , (len(list_of_integers))):
         if list_of_integers[i] > a:
