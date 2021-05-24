f36bf913af219997b02a53677a13fb6f22e6bbb0
tomi1710 <2294@holbertonschool.com>
2021-05-24 18:26:34 +0000

    fix

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- Create by Tomas de Castro for Holberton School 2021
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Create by Tomas de Castro for Holberton School 2021
+
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
